0359c32311d671b8893a4842995446f8c01ecf99
Kostadin Dimitrov <kostadin.dimitrov@telerik.com>
2017-01-04 17:59:12 +0200

    Add attractions page

--- TripXpert/TripXpert/Controllers/AttractionsController.cs
+++ TripXpert/TripXpert/Controllers/AttractionsController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TripXpert.DAL;
+using System.Collections;
 
 namespace TripXpert.Controllers
 {
@@ -12,6 +16,23 @@ namespace TripXpert.Controllers
         public ActionResult Attractions()
         {
             return View();
+        }
+
+        public ActionResult AttractionImages_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            object data = TripXpertDAL.GetAllAttractionImages();
+            return Json(((IEnumerable)data).Cast<object>().ToList().ToDataSourceResult(request));
+        }
+
+        public ActionResult AttractionInfoListView_Read([DataSourceRequest] DataSourceRequest request, string imageUrl)
+        {
+            if (string.Empty != imageUrl)
+            {
+                string url = imageUrl.Split(new char[] { '/' }).Last();
+                object data = TripXpertDAL.GetFullAttractionInfo(url);
+                return Json(((IEnumerable)data).Cast<object>().ToList().ToDataSourceResult(request));
+            }
+            return Json(string.Empty);
         }
     }
 }
---
+++ TripXpert/TripXpert/Scripts/TripXpert.js
@@ -0,0 +1,27 @@
+﻿/*global window */
+
+(function (TX, $)
+{
+    var url = "";
+    TX.showDialog = function(sender) {
+        url = sender.src;
+        var dialog = $("#dialog").data("kendoDialog");
+        var dataSource = $(dialog.element.context.firstChild).data("kendoListView").dataSource;
+        dataSource.fetch();
+        dialog.open();
+    }
+
+    TX.listViewDataBound = function()
+    {
+        var dialog = $("#dialog").data("kendoDialog");
+        if (this.dataSource.data().length != 0) {
+            dialog.center();
+            dialog.title(this.dataSource.data()[0].DestinationTitle);
+        }
+    }
+
+    TX.imageInfo = function()
+    {
+        return { imageURL: url };
+    }
+})(window.TX = window.TX || {}, $);
