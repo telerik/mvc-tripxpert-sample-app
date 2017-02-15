4b2fc590219fbeb03373a583cfdce0973659ae9d
theOrlin <theOrlin@gmail.com>
2017-02-15 15:40:25 +0200

    Add responsive menu

--- TripXpert/TripXpert/App_Start/BundleConfig.cs
+++ TripXpert/TripXpert/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace TripXpert
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/TripX.css"));
         }
     }
 }
