8e85205fa438e8601f3b2c624aee1ca010ede614
Alex Hajigeorgieva <shiatzu@gmail.com>
2018-03-21 16:34:46 +0200

    chore:update libs, replaced kendo all with only what we need

--- TripXpert/TripXpert/Scripts/TripXpert.js
+++ TripXpert/TripXpert/Scripts/TripXpert.js
@@ -4,11 +4,11 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('./service-worker.js', { scope: './' })
         .then(function (reg) {
-            console.log('Registered!')
+            console.log('Registered!');
         })
         .catch(function (err) {
             console.log(err);
-        })
+        });
 }
 
 
@@ -17,24 +17,24 @@ if ('serviceWorker' in navigator) {
     TX.showDialog = function (sender) {
         url = sender.src;
         var dialog = $("#dialog").data("kendoDialog");
-        var dataSource = $(dialog.element.context.firstChild).data("kendoListView").dataSource;
+        var dataSource = $(dialog.element).find("[data-role='listview']").data("kendoListView").dataSource;
         dataSource.fetch();
         dialog.open();
-    }
+    };
 
     TX.listViewDataBound = function () {
         var dialog = $("#dialog").data("kendoDialog");
-        if (this.dataSource.data().length != 0) {
+        if (this.dataSource.data().length !== 0) {
             dialog.center();
             dialog.title(this.dataSource.data()[0].AttractionTitle);
         }
-    }
+    };
 
     TX.imageInfo = function () {
         return { imageURL: url };
-    }
+    };
 
     TX.contact_showDialog = function () {
         $("#dialog").data("kendoDialog").open();
-    }
+    };
 })(window.TX = window.TX || {}, $);
